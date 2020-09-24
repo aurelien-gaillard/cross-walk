22e53ea1b354b7718e234f84c231ea963126c399
Aurelien Gaillard <fakeemail@gadzo.org>
2020-09-24 15:36:22 +0200

    updated

---
+++ index.js
@@ -0,0 +1,27 @@
+let timer = 0
+
+const turnLight = () => {
+    const stop = document.querySelector('.light__stop')
+    const walk = document.querySelector('.light__walk')
+    const counter = document.querySelector('.light__counter')
+    stop.innerHTML = '<img src="stop-off.svg" alt="">'
+    walk.innerHTML = '<img src="walk-on.svg" alt="">'
+    counter.textContent = '5'
+
+    const timerId = setInterval(() => {
+        timer++
+        counter.textContent = 5-timer
+        if (timer === 5) {
+            clearInterval(timerId)
+            timer = 0
+            counter.textContent = 'STOP'
+            stop.innerHTML = '<img src="stop-on.svg" alt="">'
+            walk.innerHTML = '<img src="walk-off.svg" alt="">'
+        }
+    }, 1000);
+
+}
+
+
+const btn = document.querySelector('.light__button')
+btn.addEventListener('click',turnLight)
