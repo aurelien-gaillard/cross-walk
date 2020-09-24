3b82ad05a0f47841970da5c4825f5eac447829a1
Aurelien Gaillard <fakeemail@gadzo.org>
2020-09-24 16:13:55 +0200

    updated

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-let timer = 0
+
 
 const turnLight = () => {
+    const waitingTime = 2+Math.floor(5*Math.random())
     const stop = document.querySelector('.light__stop')
     const walk = document.querySelector('.light__walk')
     const counter = document.querySelector('.light__counter')
-    stop.innerHTML = '<img src="stop-off.svg" alt="">'
-    walk.innerHTML = '<img src="walk-on.svg" alt="">'
-    counter.style.color = 'green'
-    counter.textContent = '5'
+    counter.style.color = 'orange'
+    counter.textContent = 'WAIT '+waitingTime
+    let timer = 0
     const timerId = setInterval(() => {
         timer++
-        counter.textContent = 5-timer
-        if (timer === 5) {
-            clearInterval(timerId)
-            timer = 0
-            counter.style.color = 'red'
-            counter.textContent = 'STOP'
-            stop.innerHTML = '<img src="stop-on.svg" alt="">'
-            walk.innerHTML = '<img src="walk-off.svg" alt="">'
+        if (timer < waitingTime ) {
+        counter.textContent = 'WAIT ' + (waitingTime - timer)
+        }
+        else if (timer >= waitingTime) {
+            stop.innerHTML = '<img src="stop-off.svg" alt="">'
+            walk.innerHTML = '<img src="walk-on.svg" alt="">'
+            counter.style.color = 'green'
+            counter.textContent = 'GO ' + (5 + waitingTime - timer) 
+            if (timer === (5+waitingTime)) {
+                clearInterval(timerId)
+                timer = 0
+                counter.style.color = 'red'
+                counter.textContent = 'STOP'
+                stop.innerHTML = '<img src="stop-on.svg" alt="">'
+                walk.innerHTML = '<img src="walk-off.svg" alt="">'
+            }
         }
     }, 1000)
 }
 
 
 const btn = document.querySelector('.light__button')
-btn.addEventListener('click',turnLight)
+btn.addEventListener('click', turnLight)
